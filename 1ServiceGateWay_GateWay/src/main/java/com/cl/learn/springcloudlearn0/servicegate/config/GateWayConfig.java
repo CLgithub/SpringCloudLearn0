90c2143f046d373efcf4862a3bf627bead8694bb
Lmbp <leichen070@gmail.com>
2023-02-10 17:57:32 +0800

    nacos配置中心+openFeign+sentinel+gateWay服务限流/熔断

--- 1ServiceGateWay_GateWay/src/main/java/com/cl/learn/springcloudlearn0/servicegate/config/GateWayConfig.java
+++ 1ServiceGateWay_GateWay/src/main/java/com/cl/learn/springcloudlearn0/servicegate/config/GateWayConfig.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -21,36 +22,37 @@ import java.util.List;
  * @Date 2022/11/7 16:49
  */
 @SpringBootConfiguration
+@RefreshScope
 public class GateWayConfig {
 
     // 第二种配置方式 http://ip:8101/B/m1 ---> http://vubuntu4:8902/B/m1
     @Bean
     public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder){
         return routeLocatorBuilder.routes()
-                .route("routes_B", r->r.path("/B/**").uri("lb://service-B") )
-//                .route("routes_B", (r)->{return r.path("/B/**").uri("lb://service-B");} )
+                .route("routes_B", r->r.path("/B/**").uri("lb://serviceB") )
+//                .route("routes_B", (r)->{return r.path("/B/**").uri("lb://serviceB");} )
                 .build();
     }
 
 
-    private final List<ViewResolver> viewResolvers;
-    private final ServerCodecConfigurer serverCodecConfigurer;
-
-    public GateWayConfig(ObjectProvider<List<ViewResolver>> viewResolversProvider, ServerCodecConfigurer serverCodecConfigurer) {
-        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
-        this.serverCodecConfigurer = serverCodecConfigurer;
-    }
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
-        // Register the block exception handler for Spring Cloud Gateway.
-        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
-    }
-
-    @Bean
-    @Order(-1)
-    public GlobalFilter sentinelGatewayFilter() {
-        return new SentinelGatewayFilter();
-    }
+//    private final List<ViewResolver> viewResolvers;
+//    private final ServerCodecConfigurer serverCodecConfigurer;
+//
+//    public GateWayConfig(ObjectProvider<List<ViewResolver>> viewResolversProvider, ServerCodecConfigurer serverCodecConfigurer) {
+//        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+//        this.serverCodecConfigurer = serverCodecConfigurer;
+//    }
+//
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+//        // Register the block exception handler for Spring Cloud Gateway.
+//        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+//    }
+//
+//    @Bean
+//    @Order(-1)
+//    public GlobalFilter sentinelGatewayFilter() {
+//        return new SentinelGatewayFilter();
+//    }
 }
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ springboot\springcloud学习
 
 * 注册中心：nacos
 * 服务调用：openFeign
-* 服务网关：
+* 服务网关：gateWay
 * 分布式事务：
-* 熔断/降级/限流：openFeign/gateWay2+sentinel
+* 熔断/降级/限流：openFeign/sentinel
 * 服务配置/总线：nacosConfig
 * 服务异步调用：
